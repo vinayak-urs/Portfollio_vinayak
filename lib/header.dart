5d7014ad724fbb5a938d8f3579548a667bda1ff2
in-00 <singhvinayakurs@gmail.com>
2020-07-14 01:43:54 +0530

    Font is updated

--- lib/header.dart
+++ lib/header.dart
@@ -1,64 +1,74 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 import 'package:portolliovinayak/coolors.dart';
 import 'package:velocity_x/velocity_x.dart';
-import 'package:social_media_buttons/social_media_buttons.dart';
+// import 'package:social_media_buttons/social_media_buttons.dart';
 import 'package:portolliovinayak/social.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class HeaderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var namewidget = "Singh\nVinayak"
-        .text
-        .pink300
-        .xl5
-        .lineHeight(1)
-        .size(context.isMobile ? 15 : 20)
-        .bold
-        .make()
-        .shimmer(
-            // primaryColor: Coolers.accentColor,
-            );
+    // var namewidget = "Singh\nVinayak"
+    //     .text
+    //     .pink500
+    //     .xl5
+    //     .lineHeight(1)
+    //     .size(context.isMobile ? 13 : 15)
+    //     .bold
+    //     .make()
+    //     .shimmer(
+    //         // primaryColor: Coolers.secondaryColor,
+    //         );
+
     return SafeArea(
+      // minimum: EdgeInsets.symmetric(vertical: 100.0),
       child: VxBox(
-          child: VStack([
+              child: VStack([
         ZStack([
           PictureWidget(),
           Row(
             children: [
               VStack([
-                if(context.isMobile)50.heightBox else 10.heightBox,
+                if (context.isMobile) 30.heightBox else 10.heightBox,
                 CustomAppBar().shimmer(
                   primaryColor: Coolers.accentColor,
                 ),
+                30.heightBox,
+                Text("Vinayak\nSingh",
+                    style: GoogleFonts.concertOne(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: .5,
+                        fontSize: 40,
+                      ),
+                    )),
                 // SizedBox(height: 50),
-                20.heightBox,
-                namewidget,
-                20.heightBox,
+                10.heightBox,
                 VxBox()
                     .color(Coolers.accentColor)
-                    .size(60, 10)
+                    .size(100, 10)
                     .make()
                     .shimmer(primaryColor: Coolers.accentColor),
-                10.heightBox,
+                15.heightBox,
                 SocialAccounts(),
-              ]).pSymmetric(h: context.percentWidth * 10, v: 32),
+              ]).pSymmetric(h: context.percentHeight * 2, v: 20),
               Expanded(
-                child: VxResponsive(
-                  fallback: const Offstage(),
-                  medium: Introductionwidget()
-                      .pOnly(left: 120)
-                      .h(context.percentHeight*60),
-                  large: Introductionwidget()
-                    .pOnly(left: 120)
-                    .h(context.percentHeight*60)
-                )
-              )
+                  child: VxResponsive(
+                      fallback: const Offstage(),
+                      medium: Introductionwidget()
+                          .pOnly(left: 120)
+                          .h(context.percentHeight * 60),
+                      large: Introductionwidget()
+                          .pOnly(left: 120)
+                          .h(context.percentHeight * 60)))
             ],
           ).w(context.screenWidth),
         ]),
-      ])).size(context.screenWidth, context.percentHeight*60)
+      ]))
+          .size(context.screenWidth, context.percentHeight * 45)
           .color(Coolers.secondaryColor)
           .make(),
     );
@@ -71,12 +81,19 @@ class PictureWidget extends StatelessWidget {
     return Transform(
       alignment: Alignment.topRight,
       transform: Matrix4.rotationY(0),
-      child: Image.asset(
-        "assets/p261.png",
-         fit: BoxFit.contain,
-        height: context.percentHeight*60,
-        alignment: Alignment.bottomCenter,
-      ),
+      child: Center(
+          child: Column(children: <Widget>[
+        Container(
+          margin: EdgeInsets.fromLTRB(150.0, 50.0, 0.0, 10.0),
+          width: 230,
+          height: 220,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+                image: AssetImage("assets/pic2.jpg"), fit: BoxFit.fill),
+          ),
+        ),
+      ])),
     );
   }
 }
@@ -84,10 +101,13 @@ class PictureWidget extends StatelessWidget {
 class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Icon(
-      AntDesign.codesquare,
-      size: 50,
-      color: Coolers.accentColor,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 20.0, 0, 0),
+      child: Icon(
+        AntDesign.idcard,
+        size: 70,
+        color: Coolers.accentColor,
+      ),
     );
   }
 }
@@ -96,19 +116,27 @@ class Introductionwidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return VStack([
-      ["-Introduction".text.gray500.widest.sm.make(),
+      [
+        // "-Introduction".text.gray500.widest.sm.make(),
+        Text("-Introduction",
+            style: GoogleFonts.concertOne(
+              textStyle: TextStyle(
+                color: Colors.grey,
+                letterSpacing: .5,
+                fontSize: 20,
+              ),
+            )),
         10.heightBox,
-        "@fluterdeveloper firebase ,dart & web\nMachine learning enthusiast\nFreelancer, Vlogger"
-            .text
-            .white
-            .xl3
-            .maxLines(5)
-            .make()
-            .w(context.isMobile
-            ? context.screenWidth
-            : context.percentWidth * 40),
+        Text(
+            "@fluterdeveloper firebase , dart & web ,Machine learning enthusiast , Freelancer, Vlogger",
+            style: GoogleFonts.concertOne(
+              textStyle: TextStyle(
+                color: Colors.white,
+                letterSpacing: .5,
+                fontSize: 30,
+              ),
+            )),
       ].vStack(),
     ]);
   }
 }
-
